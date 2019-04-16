b3005a48cc371193d3218faa1d5af3f34e5dcd25
PAULI <adenreleademola@yahoo.com>
2019-04-16 12:16:04 +0100

    Adding the AddReminderActivity

--- app/src/main/java/com/example/pregreminderapp/AddReminderActivity.java
+++ app/src/main/java/com/example/pregreminderapp/AddReminderActivity.java
@@ -1,13 +1,49 @@
 package com.example.pregreminderapp;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
-public class AddReminderActivity extends AppCompatActivity implements  {
+public class AddReminderActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener , DatePickerDialog.OnDateSetListener
+, LoaderManager.LoaderCallbacks<Cursor> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reminder2);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
     }
 }
